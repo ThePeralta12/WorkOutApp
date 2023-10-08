8eca3117f970a964d31add08b1a0183d902d571e
Carl Ferdinard Peralta <44725129+ThePeralta12@users.noreply.github.com>
2023-10-08 23:46:55 +0100

    Created the starting screen of the App and set up the IU.

--- app/src/main/java/com/example/workoutapp/MainActivity.kt
+++ app/src/main/java/com/example/workoutapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val flStartButton : FrameLayout = findViewById(R.id.flStart)
+
+        flStartButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "This is a test, Will start the exercise",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+
     }
 }
