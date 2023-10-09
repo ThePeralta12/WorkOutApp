f6eb6d10e4f044d6d6dad994354ebab4b9c9c52b
Carl Ferdinard Peralta <44725129+theperalta12@users.noreply.github.com>
2023-10-09 14:36:13 +0100

    Implemented Viewbinding and removed the actionbar

--- app/src/main/java/com/example/workoutapp/MainActivity.kt
+++ app/src/main/java/com/example/workoutapp/MainActivity.kt
@@ -4,23 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.example.workoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-
-        val flStartButton : FrameLayout = findViewById(R.id.flStart)
-
-        flStartButton.setOnClickListener {
+        //val flStartButton : FrameLayout = findViewById(R.id.flStart)
+        binding?.flStart?.setOnClickListener {
             Toast.makeText(
                 this@MainActivity,
                 "This is a test, Will start the exercise",
                 Toast.LENGTH_SHORT
             ).show()
         }
+    }
 
+    override fun onDestroy() { // to unassign binding for memory leak
+        super.onDestroy()
 
+        binding = null
     }
 }
