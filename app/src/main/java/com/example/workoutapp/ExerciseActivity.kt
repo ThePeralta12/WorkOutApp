a961d3212251d53c004faa5ff111778163b11385
Carl Ferdinard Peralta <44725129+theperalta12@users.noreply.github.com>
2023-10-10 20:23:32 +0100

    Implemented CountDownTimer to Display the seconds remaining

--- app/src/main/java/com/example/workoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/example/workoutapp/ExerciseActivity.kt
@@ -2,6 +2,8 @@ package com.example.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.workoutapp.databinding.ActivityExerciseBinding
 import com.example.workoutapp.databinding.ActivityMainBinding
 
@@ -9,6 +11,9 @@ class ExerciseActivity : AppCompatActivity() {
 
     private var binding: ActivityExerciseBinding? = null
 
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
+    private var tenSecondTimer = 10
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -22,7 +27,47 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
-
-
+        setupRestView()
     }
+
+    private fun setupRestView() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+        binding?.progressBar?.progress = restProgress
+
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding?.progressBar?.progress = tenSecondTimer - restProgress
+                binding?.tvTimer?.text = (tenSecondTimer - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Here now we will start the exercise",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+
+            binding = null
+        }
+    }
+
 }
