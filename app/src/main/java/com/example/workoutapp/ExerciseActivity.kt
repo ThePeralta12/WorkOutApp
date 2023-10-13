4e5cf88c14da8d2abbb154d954459c8ca9bb092b
Carl Ferdinard Peralta <44725129+theperalta12@users.noreply.github.com>
2023-10-13 16:42:36 +0100

    Added TextToSpeech onto the ExerciseActivity

--- app/src/main/java/com/example/workoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/example/workoutapp/ExerciseActivity.kt
@@ -3,11 +3,15 @@ package com.example.workoutapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.example.workoutapp.databinding.ActivityExerciseBinding
+import org.w3c.dom.Text
+import java.util.Locale
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var binding: ActivityExerciseBinding? = null
 
@@ -22,6 +26,8 @@ class ExerciseActivity : AppCompatActivity() {
     private var exerciseList: ArrayList<ExerciseModel>? = null
     private var currentExercisePosition = -1
 
+    private var tts: TextToSpeech? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -33,6 +39,8 @@ class ExerciseActivity : AppCompatActivity() {
         }
 
         exerciseList = Constants.defaultExerciseList()
+
+        tts = TextToSpeech(this, this)
 
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
@@ -55,6 +63,7 @@ class ExerciseActivity : AppCompatActivity() {
             restProgress = 0
         }
 
+
         binding?.tvUpcomingExercise?.text = exerciseList!![currentExercisePosition + 1].getName()
 
         setRestProgressBar()
@@ -74,6 +83,8 @@ class ExerciseActivity : AppCompatActivity() {
             exerciseTimer?.cancel()
             exerciseProgress = 0
         }
+
+        speakOut(exerciseList!![currentExercisePosition].getName())
 
         binding?.ivImage?.setImageResource(exerciseList!![currentExercisePosition].getImage())
         binding?.tvExerciseName?.text = exerciseList!![currentExercisePosition].getName()
@@ -126,12 +137,50 @@ class ExerciseActivity : AppCompatActivity() {
     }
 
     override fun onDestroy() {
+
+        super.onDestroy()
+
         if (restTimer != null) {
             restTimer?.cancel()
             restProgress = 0
         }
-        super.onDestroy()
+
+        if (exerciseTimer != null) {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+
+
+//        Shutting down the Text to Speech feature when activity is destroyed
+//        START
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+
         binding = null
+
+
+    }
+
+    override fun onInit(status: Int) {
+
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts?.setLanguage(Locale.UK)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported!!")
+            }
+        } else {
+            Log.e("TTS", "Invalid Error!")
+        }
+
+
+    }
+
+    private fun speakOut(text: String) {
+        tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+
     }
 
 }
