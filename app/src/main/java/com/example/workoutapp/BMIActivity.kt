d31984bf002697eb35b061d2d3d3ddd91f4b930d
Carl Ferdinard Peralta <44725129+theperalta12@users.noreply.github.com>
2023-11-27 23:04:23 +0000

    Populated BMIActivity and activity_bmi.xml

--- app/src/main/java/com/example/workoutapp/BMIActivity.kt
+++ app/src/main/java/com/example/workoutapp/BMIActivity.kt
@@ -2,7 +2,11 @@ package com.example.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.workoutapp.databinding.ActivityBmiBinding
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class BMIActivity : AppCompatActivity() {
 
@@ -17,12 +21,79 @@ class BMIActivity : AppCompatActivity() {
 
         if (supportActionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
-            supportActionBar?.title = "CALCULATOR BMI"
+            supportActionBar?.title = "CALCULATE BMI"
         }
         binding?.toolbarBmiActivity?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        binding?.btnCalculateUnits?.setOnClickListener {
+            if (validateMetricUnit()) {
+                val heightValue: Float =
+                    binding?.etMetricUnitHeight?.text.toString().toFloat() / 100
+                val weightValue: Float = binding?.etMetricUnitHeight?.text.toString().toFloat()
+
+                val bmi = weightValue / (heightValue * heightValue)
+
+                displayBMIResult(bmi)
+            } else {
+                Toast.makeText(
+                    this@BMIActivity,
+                    "Please enter valid value",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+        }
     }
 
+    private fun displayBMIResult(bmi: Float) {
 
+        val bmiLabel: String
+        val bmiDescription: String
+
+        if (bmi.compareTo(15f) <= 0) {
+            bmiLabel = "Very severely underweight!"
+            bmiDescription = "Oops! You rally need to take better care of yourself! Eat more!"
+        } else if (bmi.compareTo(15f) > 0 && bmi.compareTo(16f) <= 0) {
+            bmiLabel = "Severely underweight!"
+            bmiDescription = "Oops! You rally need to take better care of yourself! Eat more!"
+        } else if (bmi.compareTo(16f) > 0 && bmi.compareTo(18.5f) <= 0) {
+            bmiLabel = "Underweight!"
+            bmiDescription = "Oops! You rally need to take better care of yourself! Eat more!"
+        } else if (bmi.compareTo(18.5f) > 0 && bmi.compareTo(25f) <= 0) {
+            bmiLabel = "Normal"
+            bmiDescription = "Congratulations! You are in a good shape!"
+        } else if (bmi.compareTo(25f) > 0 && bmi.compareTo(30f) <= 0) {
+            bmiLabel = "Overweight"
+            bmiDescription = "Oops! You rally need to take care of yourself! Workout!"
+        } else if (bmi.compareTo(30f) > 0 && bmi.compareTo(35f) <= 0) {
+            bmiLabel = "Obese Class | (Moderately obese)"
+            bmiDescription = "Oops! You rally need to take care of yourself! Workout!!"
+        } else if (bmi.compareTo(30f) > 0 && bmi.compareTo(35f) <= 0) {
+            bmiLabel = "Obese Class || (Severely obese)"
+            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+        } else {
+            bmiLabel = "Obese Class ||| (Very Severely obese)"
+            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+        }
+
+        val bmiValue = BigDecimal(bmi.toDouble())
+            .setScale(2, RoundingMode.HALF_EVEN).toString()
+
+        binding?.llDiplayBMIResult?.visibility = View.VISIBLE
+        binding?.tvBMIValue?.text = bmiValue
+        binding?.tvBMIType?.text = bmiLabel
+        binding?.tvBMIDescription?.text = bmiDescription
+    }
+
+    private fun validateMetricUnit(): Boolean {
+        var isValid = true
+        if (binding?.etMetricUnitHeight?.text.toString().isEmpty()) {
+            isValid = false
+        } else if (binding?.etMetricUnitWeight?.text.toString().isEmpty()) {
+            isValid = false
+        }
+        return isValid
+    }
 }
